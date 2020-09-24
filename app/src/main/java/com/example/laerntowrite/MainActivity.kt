27339cc268373b52e77f7d28d9502b8217c33fa8
joni1023 <jonfu1023@gmail.com>
2020-09-24 15:13:55 -0300

    agragando estilos, mejora en comprobacion y botones

--- app/src/main/java/com/example/laerntowrite/MainActivity.kt
+++ app/src/main/java/com/example/laerntowrite/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.laerntowrite
 
+import android.app.Activity
 import android.content.Context
 import android.media.AudioManager
 import android.media.SoundPool
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.inputmethod.InputMethodManager
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
@@ -22,21 +26,49 @@ class MainActivity : AppCompatActivity() {
         iniciador()
 
 
+
         button_rep.setOnClickListener {
             mtts.speak(wordSelect, TextToSpeech.QUEUE_FLUSH, null,null)
         }
 
         buttontext.setOnClickListener {
             buttontext.requestFocus()
-            if(inputtext.text.toString() == wordSelect){
-
-                Snackbar.make(contraind,"correcto",Snackbar.LENGTH_SHORT).show()
-                clearandselect()
+            //--lower teclado
+//            val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+//            inputMethodManager.hideSoftInputFromWindow(buttontext.windowToken, 0)
+            //---
+            if(inputtext.text.toString().toLowerCase().trim() == wordSelect){
+                correcto()
             }else {
-                Snackbar.make(contraind,"incorrecto",Snackbar.LENGTH_SHORT).show()
-                clearandselect()
+                incorrecto()
             }
         }
+
+        inputtext.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                buttontext.isEnabled=p0!!.isNotEmpty()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+        })
+    }
+
+    private fun correcto() {
+        mtts.speak("correcto", TextToSpeech.QUEUE_FLUSH, null, null)
+        Snackbar.make(contraind,"correcto",Snackbar.LENGTH_SHORT).show()
+        clearandselect()
+    }
+    private fun incorrecto() {
+        mtts.speak("incorrecto", TextToSpeech.QUEUE_FLUSH, null, null)
+        Snackbar.make(contraind,"incorrecto",Snackbar.LENGTH_SHORT).show()
+        clearandselect()
     }
 
     private fun clearandselect() {
@@ -54,12 +86,13 @@ class MainActivity : AppCompatActivity() {
         })
         startList()
         selectWord()
+        buttontext.isEnabled=false
     }
 
 
     private fun startList(){
         //inicia lista
-        words= arrayOf("marciano", "pedro", "lobo", "arbol", "naranja")
+        words= arrayOf("exito", "lapiz", "choclo", "arból", "naranja","calabaza","lechuga","caramelo","loro")
 
     }
 
